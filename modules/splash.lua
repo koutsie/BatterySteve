e6d0de66d6f260d9d96836ffa88dd696f61a6ed9
koutsie <glassmc_contact@the-sauna.icu>
2025-09-21 14:31:07 +0300

    Added new warnings for BatterySteve usage + a notice about the capacity etc not maybe being accurate. I might add a countdown timer later so anxious people  don't get confused.
    - k

--- modules/splash.lua
+++ modules/splash.lua
@@ -2,6 +2,39 @@ local splash = {}
 
 function splash.show(splashes, fade_steps, delay, display_time, config)
     if not config.autotest.enabled and config.batterysteve.splashes then
+        local unskippable = {
+            "images/warnings/tool.png",
+            "images/warnings/capacity.png"
+        }
+        for i = 1, #unskippable do
+            local p = unskippable[i]
+            local img = image.load(p)
+            local custom_time = (i == 1) and 12 or 15 -- warnings *stay* on the fucking screen.
+            if not img then
+                os.message("splash failed to load: " .. p)
+            else
+                screen.clear(0)
+                screen.flip()
+                for j = 0, fade_steps do
+                    local a = math.floor((j / fade_steps) * 255)
+                    image.blit(img, 0, 0, a)
+                    screen.flip()
+                    os.delay(delay)
+                end
+                local start = os.time()
+                while os.time() - start < custom_time do
+                    image.blit(img, 0, 0, 255)
+                    screen.flip()
+                    os.delay(delay)
+                end
+                for j = fade_steps, 0, -1 do
+                    local a = math.floor((j / fade_steps) * 255)
+                    image.blit(img, 0, 0, a)
+                    screen.flip()
+                    os.delay(delay)
+                end
+            end
+        end
         for i = 1, #splashes do
             local p = splashes[i]
             local img = image.load(p)
