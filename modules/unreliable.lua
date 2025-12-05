a47df422dd19fa2120c5cc8db4d098dd5cf3a79a
koutsie <glassmc_contact@the-sauna.icu>
2025-12-06 01:06:44 +0200

    Fixed "nyan bat" / "unreliable mode" - added battery percentage tracking fixed my dumb camera mistake.
    To enabled "unreliable mode" (nyanbat mode) just create a empty file (no extension) named "NUKED" in the
    same folder as batterysteve.

--- modules/unreliable.lua
+++ modules/unreliable.lua
@@ -22,7 +22,7 @@ function unreliable.init(nukeMode, isPhatMode)
     amg.perspective(46.0)
     amg.typelight(1, __DIRECTIONAL)
     amg.colorlight(1, color.new(50, 50, 50), color.new(100, 100, 100), color.new(200, 200, 200))
-    amg.poslight(1, { 1, 4, 1 })
+    amg.poslight(1, {1, 4, 1})
 
     if not NUKE then
         Plane = model3d.load(files.cdir() .. "/3d/Data/Plane/plane.obj")
@@ -35,12 +35,12 @@ function unreliable.init(nukeMode, isPhatMode)
         return false
     end
 
-    local scaleValues = { 0.60, 0.60, 0.60 }
+    local scaleValues = {0.60, 0.60, 0.60}
     model3d.scaling(Ball, 1, scaleValues)
     model3d.shading(Ball, 1)
-    model3d.position(Ball, 1, { 0, 0.75, 0 })
-    model3d.scaling(Plane, 1, { 0.25, 0.25, 0.25 })
-    model3d.position(Plane, 1, { 0, -0.65, 0 })
+    model3d.position(Ball, 1, {0, 0.25, 0})
+    model3d.scaling(Plane, 1, {0.25, 0.25, 0.25})
+    model3d.position(Plane, 1, {0, -0.65, 0})
 
     local halfParticleSpeed = (particleMaxSpeed - particleMinSpeed) * 0.5
     for i = 1, particleCount do
@@ -66,28 +66,20 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
     for x = -1, 1, 2 do
         for y = -1, 1, 2 do
             for z = -1, 1, 2 do
-                table.insert(baseVerts, { x * half, y * half, z * half })
+                table.insert(baseVerts, {x * half, y * half, z * half})
             end
         end
     end
-    local baseEdges = {
-        { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 1 }, { 5, 6 }, { 6, 7 }, { 7, 8 }, { 8, 5 },
-        { 1, 5 }, { 2, 6 }, { 3, 7 }, { 4, 8 }
-    }
+    local baseEdges = {{1, 2}, {2, 3}, {3, 4}, {4, 1}, {5, 6}, {6, 7}, {7, 8}, {8, 5}, {1, 5}, {2, 6}, {3, 7}, {4, 8}}
 
     local function rotatePoint(p, ax, ay, az)
         return utils.rotatePoint(p, ax, ay, az)
     end
 
-    local centerSpinVerts = {
-        { 0, -30, 15 }, { 26, -15, 15 }, { 26, 15, 15 }, { 0, 30, 15 }, { -26, 15, 15 }, { -26, -15, 15 },
-        { 0, -30, -15 }, { 26, -15, -15 }, { 26, 15, -15 }, { 0, 30, -15 }, { -26, 15, -15 }, { -26, -15, -15 }
-    }
-    local centerSpinEdges = {
-        { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 6, 1 },
-        { 7, 8 }, { 8, 9 }, { 9, 10 }, { 10, 11 }, { 11, 12 }, { 12, 7 },
-        { 1, 7 }, { 2, 8 }, { 3, 9 }, { 4, 10 }, { 5, 11 }, { 6, 12 }
-    }
+    local centerSpinVerts = {{0, -30, 15}, {26, -15, 15}, {26, 15, 15}, {0, 30, 15}, {-26, 15, 15}, {-26, -15, 15},
+                             {0, -30, -15}, {26, -15, -15}, {26, 15, -15}, {0, 30, -15}, {-26, 15, -15}, {-26, -15, -15}}
+    local centerSpinEdges = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 1}, {7, 8}, {8, 9}, {9, 10}, {10, 11},
+                             {11, 12}, {12, 7}, {1, 7}, {2, 8}, {3, 9}, {4, 10}, {5, 11}, {6, 12}}
     local centerSpinRotX, centerSpinRotY, centerSpinRotZ = 0, 0, 0
     local centerSpinRotSpeedX, centerSpinRotSpeedY, centerSpinRotSpeedZ = 0.04, 0.05, 0.03
 
@@ -131,7 +123,7 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
         end
 
         local any_button = buttons.cross or buttons.circle or buttons.triangle or buttons.square or buttons.start or
-            buttons.select or buttons.l or buttons.r
+                               buttons.select or buttons.l or buttons.r
         if any_button then
             ui_visible = true
             ui_last_action = os.time()
@@ -142,7 +134,7 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
         BallRotX = BallRotX + BallRotSpeedX
         BallRotY = BallRotY + BallRotSpeedY
         BallRotZ = BallRotZ + BallRotSpeedZ
-        model3d.rotation(Ball, 1, { BallRotX * 15, BallRotY * 15, BallRotZ * 15 })
+        model3d.rotation(Ball, 1, {BallRotX * 15, BallRotY * 15, BallRotZ * 15})
 
         o = (o + 0.5) % 360
         local h = o / 60
@@ -177,20 +169,17 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
         local halfR = r * 0.5
         local halfG = g * 0.5
 
-        amg.colorlight(
-            1,
-            NUKE and color.new(128, 128, 128, 255) or color.new(r, g, b, 255),
+        amg.colorlight(1, NUKE and color.new(128, 128, 128, 255) or color.new(r, g, b, 255),
             NUKE and color.new(128, 128, 128, 128) or color.new(halfB, halfR, halfG, 128),
-            NUKE and color.new(128, 128, 128, 192) or color.new(avgR, avgG, avgB, 192)
-        )
+            NUKE and color.new(128, 128, 128, 192) or color.new(avgR, avgG, avgB, 192))
 
         currentRotation = ((currentRotation or 0) + 0.5) % 360
-        local radius = 4.75
-        local ballY = 0.75
+        local radius = 5.5
+        local ballY = 0.65
         local camX = math.cos(math.rad(currentRotation)) * radius
         local camZ = math.sin(math.rad(currentRotation)) * radius
-        cam3d.position(Camera, { camX, ballY + 1.85, camZ })
-        cam3d.eye(Camera, { 0, ballY - 2, 0 })
+        cam3d.position(Camera, {camX, ballY + 3.35, camZ})
+        cam3d.eye(Camera, {0, ballY - 2, 0})
         cam3d.set(Camera)
         model3d.render(Plane)
 
@@ -215,7 +204,9 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
 
         local now = os.time()
         local dt = now - lastUpdateTime
-        if dt > 0 then elapsedSeconds, lastUpdateTime = elapsedSeconds + dt, now end
+        if dt > 0 then
+            elapsedSeconds, lastUpdateTime = elapsedSeconds + dt, now
+        end
 
         local sec = elapsedSeconds % 60
         local min = math.floor(elapsedSeconds / 60) % 60
@@ -239,7 +230,10 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
         local verts2 = {}
         for idx, v in ipairs(baseVerts) do
             local rot = rotatePoint(v, cubeRotX, cubeRotY, cubeRotZ)
-            verts2[idx] = { x = rot[1] + cubePosX, y = rot[2] + cubePosY }
+            verts2[idx] = {
+                x = rot[1] + cubePosX,
+                y = rot[2] + cubePosY
+            }
         end
         for _, e in ipairs(baseEdges) do
             local v1, v2 = verts2[e[1]], verts2[e[2]]
@@ -249,7 +243,10 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
         local centerVerts2 = {}
         for idx, v in ipairs(centerSpinVerts) do
             local rot = rotatePoint(v, centerSpinRotX, centerSpinRotY, centerSpinRotZ)
-            centerVerts2[idx] = { x = rot[1] + 240, y = rot[2] + 136 }
+            centerVerts2[idx] = {
+                x = rot[1] + 240,
+                y = rot[2] + 136
+            }
         end
         for _, e in ipairs(centerSpinEdges) do
             local v1, v2 = centerVerts2[e[1]], centerVerts2[e[2]]
@@ -269,9 +266,8 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
             local cpuFreq = os.cpu() .. "/" .. os.bus()
             local ramUsed = math.floor(os.ram() / 1024 / 1024)
             local ramTotal = math.floor(os.totalram() / 1024 / 1024)
-            screen.print(5, 258,
-                string.format("CPU: %s MHz | RAM: %d/%d MB | %s (%s, %s)", cpuFreq, ramUsed, ramTotal, sysModel, sysGen,
-                    sysBoard), 0.40, color.new(200, 200, 200))
+            screen.print(5, 258, string.format("CPU: %s MHz | RAM: %d/%d MB | %s (%s, %s)", cpuFreq, ramUsed, ramTotal,
+                sysModel, sysGen, sysBoard), 0.40, color.new(200, 200, 200))
         end
 
         if os.time() - lastLog >= logInterval then
@@ -287,11 +283,14 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
             end
 
             if logFile then
-                logFile:write("rt:" .. timeString .. ", fps:" .. fps .. "\n")
+                local battLevel = batt and batt.lifepercent and batt.lifepercent() or nil
+                local battStr = battLevel and tostring(battLevel) or "??"
+                logFile:write("rt:" .. timeString .. ", fps:" .. fps .. ", batt:" .. battStr .. "%\n")
                 logFile:close()
             end
-
-            if power and power.tick then power.tick() end
+            if power and power.tick then
+                power.tick()
+            end
             lastLog = os.time()
         end
 
@@ -301,8 +300,12 @@ function unreliable.run(musicenabled, intro, loop, loopStarted, utils)
     end
 
     if musicenabled then
-        if intro then sound.stop(intro) end
-        if loop then sound.stop(loop) end
+        if intro then
+            sound.stop(intro)
+        end
+        if loop then
+            sound.stop(loop)
+        end
     end
 end
 
